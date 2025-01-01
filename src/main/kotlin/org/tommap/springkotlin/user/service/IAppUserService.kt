985afa7653345c2c868a045a95734d34eb4f5707
Tom <tommapcoder@gmail.com>
2025-01-01 09:48:28 +0700

    user package PII - API design

--- src/main/kotlin/org/tommap/springkotlin/user/service/IAppUserService.kt
+++ src/main/kotlin/org/tommap/springkotlin/user/service/IAppUserService.kt
@@ -1,5 +1,9 @@
 package org.tommap.springkotlin.user.service
 
+import org.tommap.springkotlin.dto.UserInfoResponse
+import org.tommap.springkotlin.dto.UserInfoUpdateRequest
+import org.tommap.springkotlin.dto.UserPasswordUpdateRequest
+
 interface IAppUserService {
     fun changeEmail(request: Map<String, String>): UserInfoResponse
     fun changePassword(request: UserPasswordUpdateRequest)
